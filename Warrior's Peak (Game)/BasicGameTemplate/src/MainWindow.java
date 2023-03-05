927bef421cc5381904eb696e77e3003c2712a199
TLobaOriginal <josogunbi@gmail.com>
2023-03-05 16:55:28 +0000

    Character bio's added

--- Warrior's Peak (Game)/BasicGameTemplate/src/MainWindow.java
+++ Warrior's Peak (Game)/BasicGameTemplate/src/MainWindow.java
@@ -48,7 +48,9 @@ public class MainWindow {
 	 private static Viewer canvas = new  Viewer(gameworld);
 	 private KeyListener Controller = new Controller();
 	 private static int TargetFPS = 100;
-	 private static boolean startGame = false;
+	 private static boolean characterBio1 = false;
+	 private static boolean characterBio2 = false;
+	 private static boolean startFight = false;
 	 private JLabel BackgroundImageForStartMenu;
 	 private final JFXPanel fxPanel = new JFXPanel();
 	 public static String resourcePath = "Warrior's Peak (Game)/BasicGameTemplate/res/";
@@ -61,20 +63,70 @@ public class MainWindow {
 		canvas.setBounds(0, 0, 880, 700);
 		canvas.setBackground(new Color(255,255,255)); 	//white background  replaced by Space background but if you remove the background method this will draw a white screen
 		canvas.setVisible(false);   							// this will become visible after you press the key.
-		       
-		JButton startMenuButton = new JButton("Start Game");// start button
-		startMenuButton.addActionListener(new ActionListener()
+
+		 JButton startGameMenuButton = new JButton("Start Game");
+		 JButton continueButton1 = new JButton("Continue");
+		 JButton continueButton2 = new JButton("Start Fight");// start button
+
+		 startGameMenuButton.addActionListener(new ActionListener() {
+			 @Override
+			 public void actionPerformed(ActionEvent e) {
+				 startGameMenuButton.setVisible(false);
+				 BackgroundImageForStartMenu.setVisible(false);
+				 File BackgroundToLoad = new File("Warrior's Peak (Game)/BasicGameTemplate/res/CharacterBio1.jpg");  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE
+				 try {
+					 BufferedImage myPicture = ImageIO.read(BackgroundToLoad);
+					 BackgroundImageForStartMenu = new JLabel(new ImageIcon(myPicture));
+					 BackgroundImageForStartMenu.setBounds(0, 0, 880, 700);
+					 frame.add(BackgroundImageForStartMenu);
+				 }  catch (IOException ex) {
+					 ex.printStackTrace();
+				 }
+
+				 frame.add(continueButton1);
+				 continueButton1.setVisible(true);
+				 frame.setVisible(true);
+			 }
+		 });
+
+
+		  continueButton1.addActionListener(new ActionListener() {
+			  @Override
+			  public void actionPerformed(ActionEvent e) {
+				  continueButton1.setVisible(false);
+				  BackgroundImageForStartMenu.setVisible(false);
+				  File BackgroundToLoad = new File("Warrior's Peak (Game)/BasicGameTemplate/res/CharacterBio2.jpg");  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE
+				  try {
+
+					  BufferedImage myPicture = ImageIO.read(BackgroundToLoad);
+					  BackgroundImageForStartMenu = new JLabel(new ImageIcon(myPicture));
+					  BackgroundImageForStartMenu.setBounds(0, 0, 880, 700);
+					  frame.add(BackgroundImageForStartMenu);
+				  }  catch (IOException ex) {
+					  ex.printStackTrace();
+				  }
+
+				  frame.add(continueButton2);
+				  continueButton2.setVisible(true);
+				  frame.setVisible(true);
+			  }
+		  });
+
+		continueButton2.addActionListener(new ActionListener()
 		{
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				startMenuButton.setVisible(false);
+				continueButton2.setVisible(false);
 				BackgroundImageForStartMenu.setVisible(false);
 				canvas.setVisible(true);
 				canvas.addKeyListener(Controller);    //adding the controller to the Canvas
 	            canvas.requestFocusInWindow();  	  // making sure that the Canvas is in focus so keyboard input will be taking in .
-				startGame=true;
+				startFight =true;
 			}});
-		startMenuButton.setBounds(400, 500, 200, 40);
+
+		 startGameMenuButton.setBounds(300, 500, 200, 70);
+		 continueButton1.setBounds(600, 500, 200, 70);
+		 continueButton2.setBounds(700, 500, 150, 50);
 	        
 		//loading background image
 		File BackgroundToLoad = new File("Warrior's Peak (Game)/BasicGameTemplate/res/startscreen.png");  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE
@@ -82,13 +134,13 @@ public class MainWindow {
 				 
 			BufferedImage myPicture = ImageIO.read(BackgroundToLoad);
 			BackgroundImageForStartMenu = new JLabel(new ImageIcon(myPicture));
-			BackgroundImageForStartMenu.setBounds(0, 0, 1000, 1000);
+			BackgroundImageForStartMenu.setBounds(0, 0, 900, 700);
 			frame.add(BackgroundImageForStartMenu);
 		}  catch (IOException e) {
 			e.printStackTrace();
 		}
 			 
-		frame.add(startMenuButton);
+		frame.add(startGameMenuButton);
 		frame.setVisible(true);
 	}
 
@@ -105,8 +157,13 @@ public class MainWindow {
 			//wait till next time step 
 		 	while (FrameCheck > System.currentTimeMillis()){}
 			
-			
-			if(startGame)
+			if(characterBio1){
+
+			}
+			if(characterBio2){
+
+			}
+			if(startFight)
 			{
 				gameOver = gameLoop();
 			}
@@ -128,53 +185,3 @@ public class MainWindow {
 	}
 
 }
-
-/*
- * 
- * 
-
-Hand shake agreement 
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,=+++
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,:::::,=+++????
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,:++++????+??
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,:,,:,:,,,,,,,,,,,,,,,,,,,,++++++?+++++????
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,=++?+++++++++++??????
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,~+++?+++?++?++++++++++?????
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:::,,,,,,,,,,,,,,,,,,,,,,,,,,,~+++++++++++++++????+++++++???????
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,,,,,,,,,,,,,,,,,,,,:===+=++++++++++++++++++++?+++????????????????
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,~=~~~======++++++++++++++++++++++++++????????????????
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,::::,,,,,,=~.,,,,,,,+===~~~~~~====++++++++++++++++++++++++++++???????????????
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,,,~~.~??++~.,~~~~~======~=======++++++++++++++++++++++++++????????????????II
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,,:=+++??=====~~~~~~====================+++++++++++++++++++++?????????????????III
-:::::::::::::::::::::::::::::::::::::::::::::::::::,:,,,++~~~=+=~~~~~~==~~~::::~~==+++++++==++++++++++++++++++++++++++?????????????????IIIII
-::::::::::::::::::::::::::::::::::::::::::::::::,:,,,:++++==+??+=======~~~~=~::~~===++=+??++++++++++++++++++++++++?????????????????I?IIIIIII
-::::::::::::::::::::::::::::::::::::::::::::::::,,:+????+==??+++++?++====~~~~~:~~~++??+=+++++++++?++++++++++??+???????????????I?IIIIIIII7I77
-::::::::::::::::::::::::::::::::::::::::::::,,,,+???????++?+?+++???7?++======~~+=====??+???++++++??+?+++???????????????????IIIIIIIIIIIIIII77
-:::::::::::::::::::::::::::::::::::::::,,,,,,=??????IIII7???+?+II$Z77??+++?+=+++++=~==?++?+?++?????????????III?II?IIIIIIIIIIIIIIIIIIIIIIIIII
-::::::::::::::::::::::::::::::,,,,,,~=======++++???III7$???+++++Z77ZDZI?????I?777I+~~+=7+?II??????????????IIIIIIIIIIIIIIIIIIIIII??=:,,,,,,,,
-::::::::,:,:,,,,,,,:::~==+=++++++++++++=+=+++++++???I7$7I?+~~~I$I??++??I78DDDO$7?++==~I+7I7IIIIIIIIIIIIIIIIII777I?=:,,,,,,,,,,,,,,,,,,,,,,,,
-++=++=++++++++++++++?+????+??????????+===+++++????I7$$ZZ$I+=~$7I???++++++===~~==7??++==7II?~,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-+++++++++++++?+++?++????????????IIIII?I+??I???????I7$ZOOZ7+=~7II?+++?II?I?+++=+=~~~7?++:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-+?+++++????????????????I?I??I??IIIIIIII???II7II??I77$ZO8ZZ?~~7I?+==++?O7II??+??+=====.,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-?????????????III?II?????I?????IIIII???????II777IIII7$ZOO7?+~+7I?+=~~+???7NNN7II?+=+=++,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-????????????IIIIIIIIII?IIIIIIIIIIII????II?III7I7777$ZZOO7++=$77I???==+++????7ZDN87I??=~,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-IIII?II??IIIIIIIIIIIIIIIIIIIIIIIIIII???+??II7777II7$$OZZI?+$$$$77IIII?????????++=+.,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII?+++?IIIII7777$$$$$$7$$$$7IIII7I$IIIIII???I+=,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII???????IIIIII77I7777$7$$$II????I??I7Z87IIII?=,,,,,,,,,,,:,,::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-777777777777777777777I7I777777777~,,,,,,,+77IIIIIIIIIII7II7$$$Z$?I????III???II?,,,,,,,,,,::,::::::::,,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-777777777777$77777777777+::::::::::::::,,,,,,,=7IIIII78ZI?II78$7++D7?7O777II??:,,,:,,,::::::::::::::,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-$$$$$$$$$$$$$77=:,:::::::::::::::::::::::::::,,7II$,,8ZZI++$8ZZ?+=ZI==IIII,+7:,,,,:::::::::::::::::,:::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-$$$I~::::::::::::::::::::::::::::::::::::::::::II+,,,OOO7?$DOZII$I$I7=77?,,,,,,:::::::::::::::::::::,,,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-::::::::::::::::::::::::::::::::::::::::::::::::::::::+ZZ?,$ZZ$77ZZ$?,,,,,::::::::::::::::::::::::::,::::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::I$:::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,:,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,,,
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,,,,
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::,,,,,,,,,,,,,,,,,,,,,,
-                                                                                                                             GlassGiant.com
- * 
- * 
- */
